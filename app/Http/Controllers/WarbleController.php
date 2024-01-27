71a747e30948e470a8b4894d5d60870ad450389c
LUIS GUSTAVO GORNIAK <gustavogorniak@gmail.com>
2024-01-27 20:20:18 -0300

    first commit

---
+++ app/Http/Controllers/WarbleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Warble;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
+
+class WarbleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('warble.index', [
+            'warbles' => Warble::with('user')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->warble()->create($validated);
+
+        return redirect(route('warble.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Warble $warble)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Warble $warble)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Warble $warble)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Warble $warble)
+    {
+        //
+    }
+}
